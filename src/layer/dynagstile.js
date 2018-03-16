b09516eb70562edf66d5e1aab23e8706d311a082
Daniel Rasmussen <dr865941@gmail.com>
2018-03-16 11:33:50 +0100

    new tile type that handles dynamic arcgis tiles.

--- src/layer/dynagstile.js
+++ src/layer/dynagstile.js
@@ -1,44 +1,74 @@
-"use strict";
-
-var ol = require('openlayers');
-var $ = require('jquery');
-var viewer = require('../viewer');
-var tile = require('./tile');
-var visibleTILELayersIDS = require('../visibleTILELayersIDS');
-
+/**
+ * This module creates a dynamic tile source that supports ArcGIS REST services with dynamic layers enabled.
+ * @type {Object} Dynamic AGS_TILE
+ */
+ "use strict";
+var ol                   = require('openlayers');
+var $                    = require('jquery');
+var viewer               = require('../viewer');
+var tile                 = require('./tile');
+/**
+ * [dynAgsTile description]
+ * @param {Object} layerOptions are passed from the configuration.
+ * @return {Object} a tile layer with a configured ags tile source. That support dynamic layers.
+ */
 var dynAgsTile = function dynAgsTile(layerOptions) {
-  var agsDefault = {
-    layerType: 'tile',
-    featureinfoLayer: undefined
-  };
-  var sourceDefault = {};
-  var agsOptions = $.extend(agsDefault, layerOptions);
-  var sourceOptions = $.extend(sourceDefault, viewer.getMapSource()[layerOptions.source]);
-  sourceOptions.attribution = agsOptions.attribution;
-  sourceOptions.projection = viewer.getProjection();
-  sourceOptions.params = agsOptions.params || {};
-  sourceOptions.params.layers = "show:" + agsOptions.id;
-  var agsSource = createSource(sourceOptions);
-
-  //console.log(sourceOptions);
-  //console.log(agsOptions);
-  //console.log(agsSource);
-
+    var agsDefault            = { layerType: 'tile', featureinfoLayer: undefined };
+    var sourceDefault         = {};
+    var agsOptions            = $.extend(agsDefault, layerOptions);
+    var sourceOptions         = $.extend(sourceDefault, viewer.getMapSource()[layerOptions.source]);
+    sourceOptions.attribution = agsOptions.attribution;
+    sourceOptions.projection  = viewer.getProjection();
+    sourceOptions.tileSize    = agsOptions.tileSize;
+    sourceOptions.params      = colligate(JSON.parse("[" + agsOptions.id + "]"));
+    var agsSource             = createSource(sourceOptions);
   return tile(agsOptions, agsSource);
-
+  /**
+   * This method creates a arcgis tile that supports REST and dynamic layers.
+   * @param {Object} options Options for source.
+   * @return {Object} return an source TileArcGISRest object
+   */
   function createSource(options) {
-    return new ol.source.TileArcGISRest({
-      attributions: options.attribution,
-      projection: options.projection,
-      crossOrigin: 'anonymous',
-      params: options.params,
-      url: options.url
-    });
-  }
-
-  function updateVisibleIDS(){
-    sourceOptions.params.layers = "show:" + visibleTILELayersIDS.ids.join();
-    return source.updateParams(sourceOptions);
+      var source = new ol.source.TileArcGISRest({
+        attributions: options.attribution,
+        projection  : options.projection,
+        crossOrigin : 'anonymous',
+        params      : options.params,
+        url         : options.url
+      });
+    return source;
   }
 }
+/**
+ * Returns a JSON object with a dynamic layers list.
+ * @param {Number} ids a list of ids.
+ * @return {Object} an JSON object with dynamicLayers as string.
+ */
+function colligate(ids){
+  return {
+    "dynamicLayers": produce(ids)
+  };
+};
+/**
+ * Returns a string representation of a list containing dynamic layers.
+ * @param {Number} ids a list of ids.
+ * @return {String} a list as string over ids.
+ */
+function produce(ids){
+    var result = "";
+    for(var drawn in ids){
+      result += (drawn == (ids.length-1)) ? reorder(drawn, ids[drawn]) : reorder(drawn, ids[drawn])+',';
+    }
+  return "["+result+"]";
+}
+/**
+ * Returns a string representation of a dynamic layer object.
+ * @param {Number} drawn the new draw position. Index 0 is the top position.
+ * @param {Number} inUse the id of the layer in the ArcGIS REST service.
+ * @return {String} a dynamic layer object.
+ */
+function reorder(drawn, inUse){
+  return JSON.stringify({"id":drawn, source: {"mapLayerId": inUse}});
+}
+
 module.exports = dynAgsTile;
