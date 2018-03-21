9ea5fa80717565375dcb556db2aa37300db4468f
Daniel Rasmussen <dr865941@gmail.com>
2018-03-21 11:29:29 +0100

    Adds comments

--- src/ids.js
+++ src/ids.js
@@ -1,2 +1,11 @@
+/**
+ * Stable array indicates nonshifting position i draworder.
+ * Static order.
+ * @type {Array}
+ */
 exports.stable = [];
+/**
+ * Selected layers has an id and is stored inside a list.
+ * @type {Array}
+ */
 exports.collected = [];
