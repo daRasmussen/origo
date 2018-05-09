dbb653f37f42d7ad233d259be78790ce60c8886f
Daniel Rasmussen <dr865941@gmail.com>
2018-05-09 16:49:24 +0200

    adds inspector

---
+++ src/inspect.js
@@ -0,0 +1,38 @@
+/**
+ * Returns an object as a String representation.
+ * @param {[Object]} v Object name.
+ * @return {[String]} Object as string.
+ */
+function objectToString(v) {
+  if (v instanceof Object) {
+    return '[' + v + ']';
+  }
+  return new Error('Can return a string of objet type.');
+}
+/**
+ * Checks property and returns default values.
+ * @param {Object} o Target object.
+ * @param {String} p Target property.
+ * @param {String} v Target value.
+ * @return {Boolean} is property set true or false.
+ */
+function hasNameWarning(o, p, v) {
+  return o.hasOwnProperty('name') ? console.warn('The ' + o.name + ' property ' + p + ' is unset. A default value has been set to ' + objectToString(v) + '.') : console.warn('The property ' + p + ' is unset. A default value has been set to ' + objectToString(v) + '.');
+}
+/**
+ * Sets the parameter value and detects if inspection is enabled and warns user with a prompt.
+ * @param {Object} obj  Target object.
+ * @param {String} prop Target property.
+ * @param {String} val  Target value.
+ * @param {Boolean} warn should user be prompted?
+ * @returns {Object} object with the new value.
+ */
+module.exports = function setInspector(obj, prop, val, warn) {
+  if (!obj.hasOwnProperty(prop)) {
+    if (warn) {
+      hasNameWarning(obj, prop, val);
+    }
+    return val;
+  }
+  return obj[prop];
+};
